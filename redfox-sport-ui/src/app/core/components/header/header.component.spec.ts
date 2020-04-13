763fe2aa1f552d8b4657e4bf1352c5e73c402e3d
Nicolai van Niekerk <nicvaniek@gmail.com>
2020-04-13 17:32:43 +0200

    Fix tests and add linting

--- redfox-sport-ui/src/app/core/components/header/header.component.spec.ts
+++ redfox-sport-ui/src/app/core/components/header/header.component.spec.ts
@@ -1,16 +1,22 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { NbSidebarService, NbUserModule, NbIconModule, NbActionsModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let sideBarService: jasmine.SpyObj<NbSidebarService>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [HeaderComponent],
+      imports: [NbUserModule, NbIconModule, NbActionsModule, NbEvaIconsModule],
+      providers: [{ provide: NbSidebarService, useValue: jasmine.createSpyObj('NbSidebarService', ['toggle']) }]
     })
-    .compileComponents();
+      .compileComponents();
+    sideBarService = TestBed.inject(NbSidebarService);
   }));
 
   beforeEach(() => {
